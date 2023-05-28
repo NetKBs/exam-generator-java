3b852e67cd82f3e4c76d9af71c25437a3c53b115
NetKBs <abijose535@gmail.com>
2023-05-28 09:54:06 -0400

    Testing interface

--- src/exam/ExamModel.java
+++ src/exam/ExamModel.java
@@ -12,11 +12,13 @@ import java.io.IOException;
 
 public final class ExamModel {
 
+
     private String topic;
     private ArrayList<Question> questions;
 
     public ExamModel(String topic) {
         this.topic = topic;
+        this.questions = new ArrayList<>();
         // Automatically generate questions about the topic
         generateQuestions();
 
@@ -51,7 +53,7 @@ public final class ExamModel {
         ArrayList<Question> code = new ArrayList<>();
 
         String slice = File.separator;
-        String file_path = System.getProperty("user.dir") + slice + "src" + slice + "questions" + slice + topic + "_questions.txt";
+        String file_path = System.getProperty("user.dir") + slice + "src" + slice + "exam" + slice + topic + "_questions.txt";
 
         try {
 
@@ -63,7 +65,7 @@ public final class ExamModel {
                 if (line.trim().isEmpty()) {
                     continue; // skip empty lines
                 }
-
+                
                 String[] line_elements = line.split("\\|"); // split the line into an array of elements using the separator |
 
                 String question = line_elements[0];
@@ -74,12 +76,14 @@ public final class ExamModel {
                 // save it
                 switch (type) {
                     case "VF":
+                     
                         vf.add(new Question(question, answer, another_answers, type));
                         break;
                     case "S1":
                         s1.add(new Question(question, answer, another_answers, type));
                         break;
                     case "S2":
+                   
                         s2.add(new Question(question, answer, another_answers, type));
                         break;
                     case "S3":
