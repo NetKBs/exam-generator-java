2dcff71b6615ab484fe92917fefabb035e5c59a0
NetKBs <abijose535@gmail.com>
2023-05-31 15:55:37 -0400

    textarea now is working and you can retrieve the reponses

--- src/GUI/PreguntasGUI.java
+++ src/GUI/PreguntasGUI.java
@@ -7,10 +7,14 @@ import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import java.util.Arrays;
 
+import java.util.Set;
+import java.util.HashSet;
+
 import javax.swing.ButtonGroup;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JTextArea;
 import javax.swing.SwingUtilities;
 import java.awt.Component;
 import javax.swing.JPanel;
@@ -22,6 +26,12 @@ import java.awt.event.ItemListener;
 
 import exam.ExamModel;
 import exam.Question;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import javax.swing.BorderFactory;
+import javax.swing.border.Border;
 
 public class PreguntasGUI extends JFrame implements ActionListener {
 
@@ -30,14 +40,16 @@ public class PreguntasGUI extends JFrame implements ActionListener {
     private CardLayout cardLayout;
     private ArrayList<Question> preguntas;
     private int preguntaActual;
+    private Question pregunta_objeto_actual;
 
     public PreguntasGUI(ArrayList<Question> preguntas) {
         super("Preguntas");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.preguntas = preguntas;
         this.preguntaActual = 0;
+    
         crearComponentes();
-        setSize(400, 300);
+        setSize(600, 500);
         setLocationRelativeTo(null);
         setVisible(true);
 
@@ -72,80 +84,98 @@ public class PreguntasGUI extends JFrame implements ActionListener {
 
         JPanel panelRespuestas = new JPanel(new GridLayout(respuestas.size(), 1));
 
-        //ButtonGroup grupoOpciones = new ButtonGroup();
-        JLabel labelPregunta = new JLabel(pregunta.getQuestion());
-        panelPregunta.add(labelPregunta, BorderLayout.NORTH);
+        // Title
+        JTextArea textAreaPregunta = new JTextArea(pregunta.getQuestion());
+        textAreaPregunta.setLineWrap(true); // Establece la posibilidad de envoltura de contenido automáticamente
+        textAreaPregunta.setWrapStyleWord(true); // Establece la posibilidad de envoltura de contenido por palabras completas
+        textAreaPregunta.setEditable(false); // Desactiva la posibilidad de editar el texto
+        panelPregunta.add(textAreaPregunta, BorderLayout.NORTH);
 
         // Agregamos los RadioButtons de cada pregunta
         for (int i = 0; i < respuestas.size(); i++) {
             String respuesta = respuestas.get(i);
-            JRadioButton radio = new JRadioButton(respuesta);
-            radio.setActionCommand(index + "-" + i);  // Nombre que identifica la opcion
+            Set<String> tipos_seleccion = new HashSet<>(Arrays.asList("VF", "S1", "S2", "S3"));
 
-            // ActionListener para el ItemEvent de los JRadioButton
-            radio.addItemListener((ItemEvent e) -> {
-                // Obtiene el JRadioButton que disparó el evento
-                JRadioButton radioSeleccionado = (JRadioButton) e.getSource();
+            // Preguntas que usan RadioButtons
+            if (tipos_seleccion.contains(pregunta.getType())) {
+                JRadioButton radio = new JRadioButton(respuesta);
+                radio.setActionCommand(index + "-" + i);  // Nombre que identifica la opcion
 
-                // Obtiene el JPanel que contiene todos los JRadioButton
-                Container container = radioSeleccionado.getParent();
-                JPanel panelRadioButtons = null;
-                // Buscamos hasta encontrar el panel padre que se instancia JPanel
-                while (container != null) {
-                    if (container instanceof JPanel) {
-                        panelRadioButtons = (JPanel) container;
-                        break;
-                    }
-                    container = container.getParent();
-                }
+                // ActionListener para el ItemEvent de los JRadioButton
+                radio.addItemListener((ItemEvent e) -> {
+                    // Obtiene el JRadioButton que disparó el evento
+                    JRadioButton radioSeleccionado = (JRadioButton) e.getSource();
 
-                // Habilita o deshabilita los JRadioButton
-                if (e.getStateChange() == ItemEvent.SELECTED) {
-                    radioSeleccionados++;
-
-                    if (("VF".equals(pregunta.getType()) || "S1".equals(pregunta.getType())) && radioSeleccionados == 1) {
-                        for (Component c : panelRadioButtons.getComponents()) {
-                            if (c instanceof JRadioButton && c != radioSeleccionado) {
-                                c.setEnabled(false);
-                            }
+                    // Obtiene el JPanel que contiene todos los JRadioButton
+                    Container container = radioSeleccionado.getParent();
+                    JPanel panelRadioButtons = null;
+                    // Buscamos hasta encontrar el panel padre que se instancia JPanel
+                    while (container != null) {
+                        if (container instanceof JPanel) {
+                            panelRadioButtons = (JPanel) container;
+                            break;
                         }
+                        container = container.getParent();
+                    }
 
-                    } else if ("S2".equals(pregunta.getType()) && radioSeleccionados == 2) {
-                        for (Component c : panelRadioButtons.getComponents()) {
+                    // Habilita o deshabilita los JRadioButton
+                    if (e.getStateChange() == ItemEvent.SELECTED) {
+                        radioSeleccionados++;
 
-                            if (c instanceof JRadioButton && c != radioSeleccionado) {
-                                JRadioButton radioButton = (JRadioButton) c; //Convert to JRadioButton to access isSelected() method
-                                if (!radioButton.isSelected()) { //Check if the radio button is not selected
-                                    radioButton.setEnabled(false);
+                        if (("VF".equals(pregunta.getType()) || "S1".equals(pregunta.getType())) && radioSeleccionados == 1) {
+                            for (Component c : panelRadioButtons.getComponents()) {
+                                if (c instanceof JRadioButton && c != radioSeleccionado) {
+                                    c.setEnabled(false);
+                                }
+                            }
+
+                        } else if ("S2".equals(pregunta.getType()) && radioSeleccionados == 2) {
+                            for (Component c : panelRadioButtons.getComponents()) {
+
+                                if (c instanceof JRadioButton && c != radioSeleccionado) {
+                                    JRadioButton radioButton = (JRadioButton) c; //Convert to JRadioButton to access isSelected() method
+                                    if (!radioButton.isSelected()) { //Check if the radio button is not selected
+                                        radioButton.setEnabled(false);
+                                    }
+                                }
+                            }
+
+                        } else if ("S3".equals(pregunta.getType()) && radioSeleccionados == 3) {
+                            for (Component c : panelRadioButtons.getComponents()) {
+
+                                if (c instanceof JRadioButton && c != radioSeleccionado) {
+                                    JRadioButton radioButton = (JRadioButton) c; //Convert to JRadioButton to access isSelected() method
+                                    if (!radioButton.isSelected()) { //Check if the radio button is not selected
+                                        radioButton.setEnabled(false);
+                                    }
                                 }
                             }
                         }
 
-                    } else if ("S3".equals(pregunta.getType()) && radioSeleccionados == 3) {
-                        for (Component c : panelRadioButtons.getComponents()) {
+                    } else { // Deselección
+                        radioSeleccionados--;
 
-                            if (c instanceof JRadioButton && c != radioSeleccionado) {
-                                JRadioButton radioButton = (JRadioButton) c; //Convert to JRadioButton to access isSelected() method
-                                if (!radioButton.isSelected()) { //Check if the radio button is not selected
-                                    radioButton.setEnabled(false);
-                                }
+                        for (Component c : panelRadioButtons.getComponents()) {
+                            if (c instanceof JRadioButton) {
+                                c.setEnabled(true);
                             }
                         }
                     }
+                });
 
-                } else {
-                    radioSeleccionados--;
+                panelRespuestas.add(radio);
 
-                    for (Component c : panelRadioButtons.getComponents()) {
-                        if (c instanceof JRadioButton) {
-                            c.setEnabled(true);
-                        }
-                    }
-                }
-            });
+            } else { // TextArea
+                JPanel subPanel = new JPanel(new GridBagLayout());
+                // crea el JTextArea con el tamaño deseado
+                JTextArea text_field = new JTextArea();
+                text_field.setPreferredSize(new Dimension(300, 100));
 
-            //grupoOpciones.add(radio);
-            panelRespuestas.add(radio);
+                // agrega el JTextArea al subpanel
+                subPanel.add(text_field, new GridBagConstraints());
+                // agrega el subpanel al contenedor principal, centrado
+                panelRespuestas.add(subPanel, new GridBagConstraints());
+            }
         }
 
         panelPregunta.add(panelRespuestas, BorderLayout.CENTER);
@@ -154,37 +184,17 @@ public class PreguntasGUI extends JFrame implements ActionListener {
         btnContinuar.setActionCommand(Integer.toString(index));
         btnContinuar.addActionListener(this);
         panelPregunta.add(btnContinuar, BorderLayout.SOUTH);
-
+            
+        pregunta_objeto_actual = pregunta;
         return panelPregunta;
     }
 
     // Manejamos el envio de preguntas y la continuación con la siguiente
-    
-     @Override
+    @Override
     public void actionPerformed(ActionEvent e) {
-        
-        /*
-        // Obtiene la respuesta seleccionada por el usuario
-        JPanel panelPreguntaActual = (JPanel) panelPrincipal.getComponent(preguntaActual);
-        ArrayList<JRadioButton> radioSeleccionado = new ArrayList<>();
 
-        // Busca el radio button seleccionado en el panel actual
-        ButtonGroup grupoOpciones = new ButtonGroup();
-        for (int i = 0; i < panelPreguntaActual.getComponentCount(); i++) {
-            if (panelPreguntaActual.getComponent(i) instanceof JPanel) {
-                JPanel panelRespuestas = (JPanel) panelPreguntaActual.getComponent(i);
-                
-                for (int j = 0; j < panelRespuestas.getComponentCount(); j++) {
-                    JRadioButton radio = (JRadioButton) panelRespuestas.getComponent(j);
-                    grupoOpciones.add(radio);
-                    if (radio.isSelected()) {
-                        radioSeleccionado.add(radio);
-                        
-                    }
-                }
-            }
-        }*/
-        
+        JTextArea textArea = null;
+
         // Obtiene la respuesta seleccionada por el usuario
         JPanel panelPreguntaActual = (JPanel) panelPrincipal.getComponent(preguntaActual);
         ArrayList<JRadioButton> radioSeleccionado = new ArrayList<>();
@@ -200,34 +210,54 @@ public class PreguntasGUI extends JFrame implements ActionListener {
                         if (radio.isSelected()) {
                             radioSeleccionado.add(radio);
                         }
+                    } else if (componentePanel instanceof JPanel) {
+                        JPanel subPanel = (JPanel) componentePanel;
+                        textArea = (JTextArea) subPanel.getComponent(0);
+
                     }
                 }
             }
         }
-        
-           // Mostramos las respuestas seleccionadas
-        ArrayList<String> respuestas = new ArrayList<>();
-        boolean hayRespuesta = false;
-        
-        for (JRadioButton radio : radioSeleccionado) { // Obtenemos los valores de las respuestas
-            String respuesta = radio != null ? radio.getText() : null;
-            if (respuesta != null) {
-                respuestas.add(respuesta);
-                hayRespuesta = true;
-            }
-        }
 
-        if (hayRespuesta) {
-            for (String respuesta : respuestas) {
-                System.out.print(respuesta);
-                if (respuestas.size() > 1) {
-                    System.out.print(" , ");
+        if (!radioSeleccionado.isEmpty()) {
+            ArrayList<String> respuestas_radio = new ArrayList<>();
+            // Mostramos las respuestas seleccionadas
+
+            boolean hayRespuesta = false;
+
+            for (JRadioButton radio : radioSeleccionado) { // Obtenemos los valores de las respuestas
+                String respuesta = radio != null ? radio.getText() : null;
+                if (respuesta != null) {
+                    respuestas_radio.add(respuesta);
+                    hayRespuesta = true;
                 }
             }
-            
-            System.out.println();
+
+            if (hayRespuesta) {
+                for (String respuesta : respuestas_radio) {
+                    System.out.print(respuesta);
+                    if (respuestas_radio.size() > 1) {
+                        System.out.print(" , ");
+                    }
+                }
+
+                System.out.println();
+            } else {
+                System.out.println("null");
+            }
+
         } else {
-            System.out.println("null");
+            if (textArea != null) {
+                String respuesta_textfield = textArea.getText();
+                if (respuesta_textfield == null || respuesta_textfield.isEmpty()) {
+                    respuesta_textfield = "";
+                }
+
+                System.out.println("Respuesta actual: " + respuesta_textfield);
+                
+            } else {
+                System.out.println("null"); // Agrega esta sentencia
+            }
         }
 
         // Avanza a la siguiente pregunta
@@ -245,14 +275,11 @@ public class PreguntasGUI extends JFrame implements ActionListener {
         this.radioSeleccionados = 0;
         cardLayout.show(panelPrincipal, idPregunta);
     }
- 
+
     public static void main(String[] args) {
-        SwingUtilities.invokeLater(new Runnable() {
-            @Override
-            public void run() {
-                ExamModel exam = new ExamModel("python");
-                PreguntasGUI preguntasGUI = new PreguntasGUI(exam.getQuestions());
-            }
+        SwingUtilities.invokeLater(() -> {
+            ExamModel exam = new ExamModel("python");
+            PreguntasGUI preguntasGUI = new PreguntasGUI(exam.getQuestions());
         });
     }
 }
