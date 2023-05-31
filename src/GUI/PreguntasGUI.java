5e0ed56daa5e02935567c2559e7b32ba836fea23
NetKBs <abijose535@gmail.com>
2023-05-31 12:44:26 -0400

    VF S1 S2 S3 RadioButtons handler

--- src/GUI/PreguntasGUI.java
+++ src/GUI/PreguntasGUI.java
@@ -1,16 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package GUI;
 
-/**
- *
- * @author Net
- */
 import java.awt.BorderLayout;
 import java.awt.CardLayout;
-import java.awt.FlowLayout;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -21,15 +11,21 @@ import javax.swing.ButtonGroup;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.SwingUtilities;
+import java.awt.Component;
 import javax.swing.JPanel;
 import javax.swing.JRadioButton;
-import javax.swing.SwingUtilities;
+import java.awt.Container;
+
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
 
 import exam.ExamModel;
 import exam.Question;
 
 public class PreguntasGUI extends JFrame implements ActionListener {
 
+    int radioSeleccionados = 0; // Variable contadora
     private JPanel panelPrincipal;
     private CardLayout cardLayout;
     private ArrayList<Question> preguntas;
@@ -44,6 +40,7 @@ public class PreguntasGUI extends JFrame implements ActionListener {
         setSize(400, 300);
         setLocationRelativeTo(null);
         setVisible(true);
+
     }
 
     private void crearComponentes() {
@@ -55,52 +52,127 @@ public class PreguntasGUI extends JFrame implements ActionListener {
         // Agregar un panel con componentes para cada pregunta
         for (int i = 0; i < preguntas.size(); i++) {
             Question pregunta = preguntas.get(i); // elemento pregunta actual
-            JPanel panelPregunta = new JPanel(new BorderLayout());
-
-            ArrayList<String> respuestas = new ArrayList<>();
-            
-            respuestas.addAll(Arrays.asList(pregunta.getAnswers()));
-            if (!pregunta.getType().equals("CD")) {
-                respuestas.addAll(Arrays.asList(pregunta.getWrongAnswers()));
-            } 
-            
-            JPanel panelRespuestas = new JPanel(new GridLayout(respuestas.size(), 1));
-
-            ButtonGroup grupoOpciones = new ButtonGroup();
-            JLabel labelPregunta = new JLabel(pregunta.getQuestion());
-            panelPregunta.add(labelPregunta, BorderLayout.NORTH);
-
-            // Agregamos los RadioButtons de cada pregunta
-            for (String respuesta : respuestas) {
-                JRadioButton radio = new JRadioButton(respuesta);
-                grupoOpciones.add(radio);
-                panelRespuestas.add(radio);
-            }
-
-            panelPregunta.add(panelRespuestas, BorderLayout.CENTER);
-
-            JButton btnContinuar = new JButton("Continuar");
-            btnContinuar.setActionCommand(Integer.toString(i));
-            btnContinuar.addActionListener(this);
-            panelPregunta.add(btnContinuar, BorderLayout.SOUTH);
-
+            JPanel panelPregunta = crearPanelPregunta(pregunta, i);
             panelPrincipal.add(panelPregunta, Integer.toString(i));
         }
 
         add(panelPrincipal);
     }
 
+    private JPanel crearPanelPregunta(Question pregunta, int index) {
+        JPanel panelPregunta = new JPanel(new BorderLayout());
+
+        // Cargamos respuestas correctas y posibles
+        ArrayList<String> respuestas = new ArrayList<>();
+
+        respuestas.addAll(Arrays.asList(pregunta.getAnswers()));
+        if (!pregunta.getType().equals("CD")) {
+            respuestas.addAll(Arrays.asList(pregunta.getWrongAnswers()));
+        }
+
+        JPanel panelRespuestas = new JPanel(new GridLayout(respuestas.size(), 1));
+
+        //ButtonGroup grupoOpciones = new ButtonGroup();
+        JLabel labelPregunta = new JLabel(pregunta.getQuestion());
+        panelPregunta.add(labelPregunta, BorderLayout.NORTH);
+
+        // Agregamos los RadioButtons de cada pregunta
+        for (int i = 0; i < respuestas.size(); i++) {
+            String respuesta = respuestas.get(i);
+            JRadioButton radio = new JRadioButton(respuesta);
+            radio.setActionCommand(index + "-" + i);  // Nombre que identifica la opcion
+
+            // ActionListener para el ItemEvent de los JRadioButton
+            radio.addItemListener((ItemEvent e) -> {
+                // Obtiene el JRadioButton que disparó el evento
+                JRadioButton radioSeleccionado = (JRadioButton) e.getSource();
+
+                // Obtiene el JPanel que contiene todos los JRadioButton
+                Container container = radioSeleccionado.getParent();
+                JPanel panelRadioButtons = null;
+                // Buscamos hasta encontrar el panel padre que se instancia JPanel
+                while (container != null) { 
+                    if (container instanceof JPanel) {
+                        panelRadioButtons = (JPanel) container;
+                        break;
+                    }
+                    container = container.getParent();
+                }
+
+                // Habilita o deshabilita los JRadioButton
+                if (e.getStateChange() == ItemEvent.SELECTED) {
+                    radioSeleccionados++;
+
+                    if (("VF".equals(pregunta.getType()) || "S1".equals(pregunta.getType())) && radioSeleccionados == 1) {
+                        for (Component c : panelRadioButtons.getComponents()) {
+                            if (c instanceof JRadioButton && c != radioSeleccionado) {
+                                c.setEnabled(false);
+                            }
+                        }
+                        
+                    } else if ("S2".equals(pregunta.getType()) && radioSeleccionados == 2) {
+                        for (Component c : panelRadioButtons.getComponents()) {
+                            
+                            if (c instanceof JRadioButton && c != radioSeleccionado) {
+                                JRadioButton radioButton = (JRadioButton) c; //Convert to JRadioButton to access isSelected() method
+                                if (!radioButton.isSelected()) { //Check if the radio button is not selected
+                                    radioButton.setEnabled(false);
+                                }
+                            }
+                        }
+                        
+                    } else if ("S3".equals(pregunta.getType()) && radioSeleccionados == 3) {
+                        for (Component c : panelRadioButtons.getComponents()) {
+                            
+                            if (c instanceof JRadioButton && c != radioSeleccionado) {
+                                JRadioButton radioButton = (JRadioButton) c; //Convert to JRadioButton to access isSelected() method
+                                if (!radioButton.isSelected()) { //Check if the radio button is not selected
+                                    radioButton.setEnabled(false);
+                                }
+                            }
+                        }
+                    }
+
+                } else {
+                    radioSeleccionados--;
+
+                    for (Component c : panelRadioButtons.getComponents()) {
+                        if (c instanceof JRadioButton) {
+                            c.setEnabled(true);
+                        }
+                    }
+                }
+            });
+
+            //grupoOpciones.add(radio);
+            panelRespuestas.add(radio);
+        }
+
+        panelPregunta.add(panelRespuestas, BorderLayout.CENTER);
+
+        JButton btnContinuar = new JButton("Continuar");
+        btnContinuar.setActionCommand(Integer.toString(index));
+        btnContinuar.addActionListener(this);
+        panelPregunta.add(btnContinuar, BorderLayout.SOUTH);
+
+        return panelPregunta;
+    }
+
+    // Manejamos el envio de preguntas y la continuación con la siguiente
     @Override
     public void actionPerformed(ActionEvent e) {
         // Obtiene la respuesta seleccionada por el usuario
         JPanel panelPreguntaActual = (JPanel) panelPrincipal.getComponent(preguntaActual);
         JRadioButton radioSeleccionado = null;
 
+        // Busca el radio button seleccionado en el panel actual
+        ButtonGroup grupoOpciones = new ButtonGroup();
         for (int i = 0; i < panelPreguntaActual.getComponentCount(); i++) {
             if (panelPreguntaActual.getComponent(i) instanceof JPanel) {
                 JPanel panelRespuestas = (JPanel) panelPreguntaActual.getComponent(i);
                 for (int j = 0; j < panelRespuestas.getComponentCount(); j++) {
                     JRadioButton radio = (JRadioButton) panelRespuestas.getComponent(j);
+                    grupoOpciones.add(radio);
                     if (radio.isSelected()) {
                         radioSeleccionado = radio;
                         break;
@@ -121,9 +193,10 @@ public class PreguntasGUI extends JFrame implements ActionListener {
         }
 
         // Obtiene la ID de la siguiente pregunta
-        String idPregunta = Integer.toString(preguntaActual + 1);
+        String idPregunta = Integer.toString(preguntaActual);
 
         // Muestra la siguiente pregunta
+        this.radioSeleccionados = 0;
         cardLayout.show(panelPrincipal, idPregunta);
     }
 
